09dfd6746b411f78c8637803590a0f9e92f279c1
Andre Soares Gomes <andrsoares953@yahoo.com>
2024-03-31 23:51:14 -0300

    Update UpdateFromDB.cs

--- CSharp/EFcore/UpdateFromDB.cs
+++ CSharp/EFcore/UpdateFromDB.cs
@@ -7,3 +7,20 @@ var rowsAffected = await dbContext.Products
     .ExecuteUpdateAsync(p => new Product {Price = p.Price * 1.1});
 
 Console.WriteLine($"{rowsAffected} rows were updated.");
+
+//Do update using update API and property casting
+context.Blogs
+    .Where(b => b.Rating < 3)
+    .ExecuteUpdate(setters => setters.SetProperty(b => b.IsVisible, false));
+
+//More than one property
+context.Blogs
+    .Where(b => b.Rating < 3)
+    .ExecuteUpdate(setters => setters
+        .SetProperty(b => b.IsVisible, false)
+        .SetProperty(b => b.Rating, 0));
+
+//Update using the property value itself as reference
+context.Blogs
+    .Where(b => b.Rating < 3)
+    .ExecuteUpdate(setters => setters.SetProperty(b => b.Rating, b => b.Rating + 1));
